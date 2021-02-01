f93f930bbe916b9bc5befb36287dce332cb483ac
dayfundora <dayfundoraglez@gmail.com>
2021-02-01 01:57:04 -0500

    make Corpus LIWC

--- src/logic/liwc_vectorizer.py
+++ src/logic/liwc_vectorizer.py
@@ -14,6 +14,24 @@ class LIWCVectorizer:
         vector = [0]*len(self.features)
         for v in parse:
             vector[self.featuresidx[v]] = parse[v]/len(doc)
-            #print('feature = {0}   value = {1}'.format(v,vector[self.featuresidx[v]]))
         return vector
 
+    def vectorize_docs(self, documents):
+        vectors = []
+        for doc in documents:
+            vectors.append(self.vectorize(doc))
+        return vectors
+
+    def make_corpus_liwc(self, corpus_path):
+        df_essays = pd.read_csv(corpus_path, encoding="ISO-8859-1")
+        corpus = df_essays['STATUS']
+        vector = self.vectorize_docs(corpus)
+
+        dataframe = pd.DataFrame(vector, columns=self.features)
+        
+        scores = df_essays.loc[:, ['sEXT','sNEU','sAGR','sCON','sOPN','cEXT','cNEU','cAGR','cCON','cOPN']]
+        dataframe = dataframe.join(scores)
+
+        writer = pd.ExcelWriter('./data/liwc_corpus.xlsx', engine='xlsxwriter')
+        dataframe.to_excel(writer, sheet_name='Sheet1')
+        writer.save()
