ae38e2361ba8a09f697ec315d245d387d1dbb449
dayfundora <dayfundoraglez@gmail.com>
2021-02-12 00:24:33 -0500

    Learning Curves

--- src/logic/plotters.py
+++ src/logic/plotters.py
@@ -53,3 +53,43 @@ def plot_to2(X, y):
     plt.scatter(to_plot[:,0], to_plot[:,1], c=y, cmap=plt.cm.Paired)
     plt.title('Actual labels')
     plt.show()
+
+def learning_curves(estimator, X, y, cv, trait, vectorizer):
+    start = time.time()
+    train_sizes, train_scores, validation_scores = learning_curve(
+    estimator, X, y, cv = cv, scoring = 'accuracy', verbose=1)
+    end = time.time()
+
+    train_scores_mean = train_scores.mean(axis = 1)
+    validation_scores_mean = validation_scores.mean(axis = 1)
+
+    train_series_mean = pd.Series(train_scores_mean, index = train_sizes)
+    validation_series_mean = pd.Series(validation_scores_mean, index = train_sizes)
+
+    
+    print('train_size = {0}'.format(train_sizes))
+    print('Mean training scores\n\n', train_series_mean)
+    print('-' * 20)
+    print('\nMean validation scores\n\n', validation_series_mean)
+    
+
+    m1 = train_scores_mean.mean()
+    m2 = validation_scores_mean.mean()
+    model_score = (m1 + m2)/2
+
+    delay = start -  end
+
+    
+    plt.plot(train_sizes, train_scores_mean, label = 'Training error')
+    plt.plot(train_sizes, validation_scores_mean, label = 'Validation error')
+
+    plt.ylabel('Accuracy', fontsize = 14)
+    plt.xlabel('Training set size', fontsize = 14)
+    title = 'Learning curves for a ' + str(estimator).split('(')[0] + ' model for {0} trait vectorizing with {1} score = {2}'.format(trait,vectorizer, model_score)
+    plt.title(title, fontsize = 18, y = 1.03)
+    plt.legend()
+    plt.ylim(-2,3)
+    plt.savefig('{0}_{1}_chart.png'.format(trait,vectorizer))
+    plt.show()
+
+    return model_score
