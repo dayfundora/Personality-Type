6b788fd087656e6661dfb92c8877c2892d1d40fa
dayfundora <dayfundoraglez@gmail.com>
2021-02-15 02:57:43 -0500

    Save Log

--- src/logic/Model_Evaluation.py
+++ src/logic/Model_Evaluation.py
@@ -53,6 +53,7 @@ class Model_Evaluation():
     def compare_scores(self, save):
         
         print('Model performance for trait ' + self.trait + ' prediction:' + '\n')
+        save.write("'Model performance for trait ' + self.trait + ' prediction:' + '\n'")           
         
         accuracy_scores = []
         f1_scores = []
@@ -62,24 +63,32 @@ class Model_Evaluation():
             model.fit(self.X_train, self.y_train)
 
             print(model_name + ": ")
+            save.write(model_name + ": ")
 
             if regression:
                 y_pred = model.predict(self.X_test)
                 y_true = self.y_test
                 mse = -np.mean(cross_validate(model, self.X_test, self.y_test, scoring='neg_mean_squared_error', cv=10)['test_score'])
                 print('MSE: ' + str(mse) + '\n')
+                save.write('MSE: ' + str(mse) + '\n')
             else:
                 accuracy_score = np.mean(cross_validate(model, self.X_test, self.y_test, cv=10)['test_score'])
                 accuracy_scores.append(accuracy_score)
                 print('Accuracy score: ' + str(accuracy_score) + '\n')
+                save.write('Accuracy score: ' + str(accuracy_score) + '\n')
 
                 f_score = np.mean(cross_validate(model, self.X_test, self.y_test, scoring='f1', cv=10)['test_score'])
                 f1_scores.append(f_score)
                 print('F1 score: ' + str(f_score) + '\n')
+                save.write(('F1 score: ' + str(f_score) + '\n'))
 
         best_accuracy_score = max(accuracy_scores)
         best_accuracy_model, d = self.models[accuracy_scores.index(best_accuracy_score)]
         print(
+            '*****Best Accuracy score for ' + str(self.trait) + ': '  + str(best_accuracy_score) + '\n' +
+            'Model: ' + best_accuracy_model + '\n' + '\n'
+        )
+        save.write(
             '*****Best Accuracy score for ' + str(self.trait) + ': '  + str(best_accuracy_score) + '\n' +
             'Model: ' + best_accuracy_model + '\n' + '\n'
         )
@@ -89,7 +98,12 @@ class Model_Evaluation():
             '*****Best F1 score for ' + str(self.trait) + ': ' + str(best_f1_score) + '\n' +
             'Model: ' + best_f1_model + '\n\n\n'
         )
+        save.write(
+            '*****Best F1 score for ' + str(self.trait) + ': ' + str(best_f1_score) + '\n' +
+            'Model: ' + best_f1_model + '\n\n\n'
+        )
         print('----------End Model performance for trait ' + self.trait + ' prediction-------------' + '\n')
+        save.write('----------End Model performance for trait ' + self.trait + ' prediction-------------' + '\n')
 
 
 def prep_data(trait,dp, regression=False, model_comparison=False):
