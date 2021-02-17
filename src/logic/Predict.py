68746c79ca15f683faa678dc071724302bd87e1b
dayfundora <dayfundoraglez@gmail.com>
2021-02-17 00:32:12 -0500

    Update Predict.py

--- src/logic/Predict.py
+++ src/logic/Predict.py
@@ -27,3 +27,25 @@ class Predictor():
                 #sys.path.append(r'filename')
             with open('logic/models/{1}_categorical_model_{0}.pkl'.format(self.vetorice, trait), 'rb') as f:
                 self.models[trait] = pickle.load(f)
+
+    def predict(self, X, traits='All', predictions='All'):
+        predictions = {}
+        if traits == 'All':
+            for trait in self.traits:
+                pkl_model = self.models[trait]
+                
+                trait_scores = pkl_model.predict(X, regression=True).reshape(1, -1)
+                predictions['pred_s'+trait] = trait_scores.flatten()[0]
+
+                
+                trait_categories = pkl_model.predict(X, regression=False)
+                predictions['pred_c'+trait] = str(trait_categories[0])
+
+               
+                trait_categories_probs = pkl_model.predict_proba(X)
+                predictions['pred_prob_c'+trait] = trait_categories_probs[:, 1][0]
+            
+            result = self.print_Pred(predictions, X)
+
+        return result
+    
