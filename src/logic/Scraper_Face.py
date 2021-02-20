fbf352326f887f70f672f322bd1c5a06e7377943
dayfundora <dayfundoraglez@gmail.com>
2021-02-20 00:43:25 -0500

    Search User Statuses in Facebook

--- src/logic/Scraper_Face.py
+++ src/logic/Scraper_Face.py
@@ -29,6 +29,7 @@ class FBScraper():
             options = ChromeOptions()
             options.add_argument("--disable-notifications")
             self.browser = Chrome(options=options)
+    
     def open_fb(self):
         #Login to FB in Selenium Browser
         url = 'http://www.facebook.com/'
@@ -42,4 +43,67 @@ class FBScraper():
 
         self.browser.find_element_by_id("loginbutton").click()
 
+def searched_statuses(self):
+        #self.browser.find_element_by_css_selector(f'a[href="{self.first_profile_url}"]').click()
+        #time.sleep(self.scroll_time)
+
+        # Data structure of entry:
+        #   {'name': STRING = name,
+        #   'url': STRING = profile url,
+        #   'datetime': DATETIME = current time,
+        #   'statuses': DICT = {key=time of status post, value=status},
+        #   'html': STRING = html of page,}
+        #print("Url Anterior: " + str(self.browser.current_url))
+        
+        self.browser.get(self.first_profile_url)
+        #print("Url Actual: " + str(self.browser.current_url))
+        #input("Para")
+        time.sleep(self.scroll_time)
+
+        #Get scroll heigth
+        #last_heigth = self.browser.execute_script("return document.body.scrollHeight")
+
+        #Scroll through timeline and add statuses to dictionary
+        #while len(self.dic_status.keys()) < self.number_status:
+
+        Scroll_Pause_Time = self.scroll_time * (1 + random.random())
+
+        #Scroll down to bottom
+        # self.browser.execute_script("Window.scrollTo(0, document.body.scrollHeight);")
+
+        #Weait to load page
+        #time.sleep(Scroll_Pause_Time)
+
+        print("--------Searching Post--------")
+        #All_Post = self.browser.find_elements_by_tag_name('a')
+        All_Post = self.browser.find_element_by_css_selector("div[id^='tl_unit_']")
+                    
+        #input("All_Post: " + str(All_Post))
+
+        #for post in All_Post:
+        try:       
+            post_time_element = All_Post.find_element_by_css_selector('abbr')
+            post_time = post_time_element.get_attribute('title')
+            user_content_element = All_Post.find_element_by_css_selector("div.userContent")         
+            para_elements = user_content_element.find_element_by_css_selector('p')
+            #Status sometimes split in two p elements. Merge two paragraphs
+            if para_elements is not None:
+                text = ''            
+                text += para_elements.text + ' '
+            print('Date: ' + post_time + '\n' + 'Status: ' + text + '\n')
+        
+                #Add status to dictionary
+            self.dic_status[post_time] = text
+        except:
+            print("Elements Not Found")
+
+        print("Finished creating Statuses for: " + str(self.name))
+
+        print("Final Dic: " + str(self.dic_status))
+
+        save = open ('logic/data/post.txt', 'w')
+        save.write(text)
+        save.close()   
+
+    
    
