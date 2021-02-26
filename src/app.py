ef7ea0bc908aac78722e07aa0860246e148a70a8
dayfundora <dayfundoraglez@gmail.com>
2021-02-26 01:06:17 -0500

    Create app.py

---
+++ src/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, url_for, redirect
+from logic.Model import Model,train_models
+from logic.Predict import Predictor
+from logic.Scraper_Face import  FBScraper, ExecFace
+from bson import json_util
+import json
+import yaml
+import pickle
+
+UPLOAD_FOLDER = 'static/img'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(debug=True)
